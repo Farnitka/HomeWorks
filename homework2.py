214b3952e76a1d5c59c13b03a8a5c5b84ee53384
Farnitka <119747561+Farnitka@users.noreply.github.com>
2024-08-02 21:39:17 +0300

    Переменные
    Напишите 4 переменных которые буду обозначать следующие данные:
    Количество выполненных ДЗ (запишите значение 12)
    Количество затраченных часов (запишите значение 1.5)
    Название курса (запишите значение 'Python')
    Время на одно задание (вычислить используя 1 и 2 переменные)
    Выведите на экран(в консоль), используя переменные, следующую строку:
    Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+all_time_count = 1.5
+name_of_course = "Python"
+time_of_one_work = all_time_count/homework_count
+print('Курс:',name_of_course,', всего задач: ', homework_count,', затрачено часов: ',all_time_count,', среднее время выполнения ',time_of_one_work, ' часа.')
